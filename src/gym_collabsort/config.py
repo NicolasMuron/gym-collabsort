89cdc36bd0c4f5338391a8b8b48990b64919a9b6
NicolasMuron <nicolas.muron@gmail.com>
2026-06-26 17:39:27 +0200

    Test pour 500 episodes du DQN classqiue

--- src/gym_collabsort/config.py
+++ src/gym_collabsort/config.py
@@ -205,7 +205,7 @@ class Config:
     enable_reward_change: bool = False
 
     # Step threshold at which the agent reward matrix is switched
-    reward_change_step: int = 100000
+    reward_change_step: int = 250000
 
     agent_rewards_after: np.ndarray = field(
         default_factory=lambda: np.array([[3, 4, 5], [0, 1, 2], [6, 7, 8]])
